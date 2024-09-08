e745a5d6b360cef273aeaa78b45a8d0dcd899c90
Rodney Finkel <111357994+RodneyFinkel@users.noreply.github.com>
2024-09-09 01:48:18 +0300

    sentiment_analysis fix

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,3 +1,4 @@
+from finvizfinance.screener.overview import Overview
 from transformers import pipeline
 import yfinance as yf
 from goose3 import Goose 
@@ -6,6 +7,40 @@ import pandas as pd
 import csv
 import os
 import json
+
+
+def get_undervalued_stocks():
+    foverview = Overview()
+    filters_dict = {'Debt/Equity':'Under 1', 
+                    'PEG':'Low (<1)', 
+                    'Operating Margin':'Positive (>0%)', 
+                    'P/B':'Low (<1)',
+                    'P/E':'Low (<15)',
+                    'InsiderTransactions':'Positive (>0%)'}
+    parameters = ['Exchange', 'Index', 'Sector', 'Industry', 'Country', 'Market Cap.',
+        'P/E', 'Forward P/E', 'PEG', 'P/S', 'P/B', 'Price/Cash', 'Price/Free Cash Flow',
+        'EPS growththis year', 'EPS growthnext year', 'EPS growthpast 5 years', 'EPS growthnext 5 years',
+        'Sales growthpast 5 years', 'EPS growthqtr over qtr', 'Sales growthqtr over qtr',
+        'Dividend Yield', 'Return on Assets', 'Return on Equity', 'Return on Investment',
+        'Current Ratio', 'Quick Ratio', 'LT Debt/Equity', 'Debt/Equity', 'Gross Margin',
+        'Operating Margin', 'Net Profit Margin', 'Payout Ratio', 'InsiderOwnership', 'InsiderTransactions',
+        'InstitutionalOwnership', 'InstitutionalTransactions', 'Float Short', 'Analyst Recom.',
+        'Option/Short', 'Earnings Date', 'Performance', 'Performance 2', 'Volatility', 'RSI (14)',
+        'Gap', '20-Day Simple Moving Average', '50-Day Simple Moving Average',
+        '200-Day Simple Moving Average', 'Change', 'Change from Open', '20-Day High/Low',
+        '50-Day High/Low', '52-Week High/Low', 'Pattern', 'Candlestick', 'Beta',
+        'Average True Range', 'Average Volume', 'Relative Volume', 'Current Volume',
+        'Price', 'Target Price', 'IPO Date', 'Shares Outstanding', 'Float']
+    
+    foverview.set_filter(filters_dict=filters_dict)
+    df_overview = foverview.screener_view()
+    if not os.path.exists('overview_out'):
+        os.makedirs('overview_out')
+    df_overview.to_csv('overview_out/Overview.csv', index=False)
+    tickers = df_overview['Ticker'].to_list()
+    return tickers
+
+
 
 def get_ticker_news_sentiment(ticker):
     ticker_news = yf.Ticker(ticker)
@@ -59,7 +94,7 @@ def generate_csv_and_json(ticker):
     df.to_json(f'out/{ticker}.json', orient='records')
 
 if __name__ == '__main__':
-    ticker_list = ['LMT', 'AAPL', 'GE', 'META', 'TSLA', 'MSFT', 'GOOGL']
+    ticker_list = get_undervalued_stocks()
     for i in ticker_list:
         generate_csv_and_json(i)
     
